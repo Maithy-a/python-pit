8ea00b5030b0af2e157e9f1d2804f62c580ea481
Maithya <maithyabon2@gmail.com>
2025-03-02 03:05:40 +0300

    Refine square root finder input prompts and error messages in sroot.py

--- sroot.py
+++ sroot.py
@@ -1,21 +1,15 @@
-#a = 6
-#b = 3
-#a /= 2 * b
-#print(a)
-
-
 
 #square root finder
 import  math
 def square ():
     while True:
         try:
-            val = float(input("Enter a value to find its Square root:"))
+            val = float(input("\nEnter a value to find its Square root:"))
             if val < 0:
                 print("Cannot find square roots of negative numbers")
             else:
                 sroot = math.sqrt(val)
                 print(f"The square root of {val} is: {sroot:.2f}")
         except ValueError:
-            print("Enter a valid number")
+            print("Enter a valid Value")
 square()
